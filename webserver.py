4c4eec8ff31fa53ecd18286ea41e8bc945df40df
Solidx74 <kareebsadab@gmail.com>
2025-11-20 19:45:37 +0600

    webserver.py

--- webserver.py
+++ webserver.py
@@ -1,4 +1,4 @@
-from flask import flask, Flask
+from flask import Flask
 from threading import Thread
 
 app = Flask('')
@@ -12,4 +12,5 @@ def run():
 def keep_alive():
     t=Thread(target=run)
     t.start()
+
 
